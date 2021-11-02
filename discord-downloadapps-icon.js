7969414e7eb5deaecfdbb0425b7413dd86533aba
BE-CH <mikkel.l.bech@gmail.com>
2021-11-02 16:56:27 +0100

    + added support for multiple languages

--- discord-downloadapps-icon.js
+++ discord-downloadapps-icon.js
@@ -1,6 +1,8 @@
 // This is the download app icon
 function findDownloadAppsIconWrapper(stopElement) {
-	let downloadAppsSVG = document.querySelector('[aria-label="Download Apps"]');
+	let downloadAppsSVG = document.querySelector(
+		'[data-list-item-id="guildsnav___app-download-button"]'
+	);
 
 	while (
 		downloadAppsSVG !== null &&
@@ -45,6 +47,7 @@ function findGuildSepeartor(startElement) {
 
 let removed = false;
 function tryRemoveAppIcon() {
+	console.log('-> Started to search for download button');
 	setTimeout(function () {
 		// Getting the top element in the SideBar and using that as a stopper for the loops.
 		// This will make sure that if we cant find the data, then it wont run forever.
@@ -79,12 +82,21 @@ tryRemoveAppIcon();
 
 document.addEventListener('click', (e) => {
 	// when the user clicks log out then start searching again!
-	if (
-		e.target &&
-		e.target.getAttribute('type') === 'submit' &&
-		e.target.innerText.trim() === 'Log Out'
-	) {
-		removed = false;
-		tryRemoveAppIcon();
+	if (e.target) {
+		// check if its the button that is clicked or if its the div inside the button
+		const clicked =
+			e.target.nodeName === 'BUTTON' ? e.target : e.target.parentElement;
+
+		if (
+			clicked &&
+			clicked.nodeName === 'BUTTON' &&
+			clicked.getAttribute('type') === 'submit' &&
+			/^.*?\bbutton-\b.*?\blookFilled-\b.*?colorRed-\b.*?sizeMedium-\b.*?grow-\b.*?$/.test(
+				clicked.className
+			)
+		) {
+			removed = false;
+			tryRemoveAppIcon();
+		}
 	}
 });
